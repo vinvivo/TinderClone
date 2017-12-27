acba989c943a38cfc6ab763cc97765e640846653
vinney <vinvivo@users.noreply.github.com>
2017-12-27 00:21:28 -0800

    created container for booklist

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
+import BooksReducer from './reducer_books';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  // as per diagram from video lecture, our application has a single piece of state called "books" with a value equal to whatever is returned from the imported BooksReducer function
+  books: BooksReducer
 });
 
 export default rootReducer;
