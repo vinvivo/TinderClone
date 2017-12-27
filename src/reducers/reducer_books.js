1cbb7a74166dc4cfb45f898255d922de02295e0d
vinney <vinvivo@users.noreply.github.com>
2017-12-27 00:08:49 -0800

    Add reducer to return book title state

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+// To make use of this reduced, we must export this function by writing "export default" before the function
+
+export default function() {
+    return [
+        {title: 'Javascript: The Best Parts'},
+        {title: 'Hairy Otter'},
+        {title: 'The Bright Light'},
+        {title: 'Cave of Wonders'}
+    ]
+}
